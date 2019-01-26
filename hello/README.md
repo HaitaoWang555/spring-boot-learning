1ed5ee26a9c076c0a93335b5061fe9a42338af34
wanghaitao <15124505701@163.com>
2019-01-27 07:31:51 +0800

    hello spring boot

---
+++ hello/README.md
@@ -0,0 +1,9 @@
+## 新建 spring boot 项目
+
+1. 在[官网](https://start.spring.io/)进行配置,在导入idea
+
+2. 在pom文件中注释掉`spring-boot-starter-tomcat`
+
+3. 新建Hello类
+
+4. 启动项目,浏览器访问`http://localhost:8080/hello` 得到`hello spring boot`
---
+++ hello/src/main/java/com/blog/hello/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.blog.hello;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
+
